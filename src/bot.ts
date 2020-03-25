8715d584c0368b3961c7531ac237bfc07f8d26ab
Gennerator <VladutzBoy2010@Gmail.Com>
2020-03-25 15:39:10 +0200

    Refactor switch statement into multiple 'if-return' statements

--- src/bot.ts
+++ src/bot.ts
@@ -3,6 +3,8 @@ import ytdl = require('ytdl-core-discord');
 const client = new Discord.Client();
 const { prefix, token } = require('./config.json');
 
+let isPlaying = false;
+
 client.on('ready', () => {
   if (client.user !== null) {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -21,70 +23,70 @@ client.on('message', (msg: Discord.Message) => {
   const param = split.join(' ');
 
   console.log(`[COMMAND] ${command} [PARAM] ${param}`);
-  switch (command) {
-    case `repeta`:
-      if (param.length > 0) {
-        msg.channel.send(`**Matale ai spus:** ${param}`);
-      } else {
-        msg.channel.send(`Păi dă-mi un mesaj să îl repet, băi! <:cmonBruh:646737462256992296>`);
-      }
-      break;
-    case 'play': case 'p':
-      if (param.length > 0) {
-        if (msg.member.voice.channel !== null) {
-          if (ytdl.validateURL(param) === true) {
-            playback(msg.member.voice.channel, param);
-            ytdl.getBasicInfo(param)
-              .then((info) => {
-                const minutes = Math.floor(info.player_response.videoDetails.lengthSeconds / 60);
-                const seconds = info.player_response.videoDetails.lengthSeconds % 60;
 
-                msg.channel.send(
-                  new Discord.MessageEmbed()
-                    .setColor('#00FF00')
-                    .setAuthor('--------------- ÎN CURS DE REDARE ---------------')
-                    .setTitle(info.player_response.videoDetails.title)
-                    .addFields(
-                      { name: 'Adăugat de', value: `<@${msg.author.id}>`, inline: true },
-                      { name: 'Durata', value: `${minutes}:${seconds}`, inline: true },
-                      {
-                        name: 'Link YouTube',
-                        value: `https://www.youtube.com/watch?v=${info.player_response.videoDetails.videoId}`,
-                      }
-                    )
-                );
-              })
-              .catch(console.log);
+  if (command === 'repeta') {
+    if (param.length > 0) {
+      msg.channel.send(`**Matale ai spus:** ${param}`);
+    } else {
+      msg.channel.send(`Păi dă-mi un mesaj să îl repet, băi! <:cmonBruh:646737462256992296>`);
+    }
+    return;
+  }
 
-          } else {
-            msg.channel.send(
-              new Discord.MessageEmbed()
-                .setColor('#FF0000')
-                .setTitle('Link-ul introdus este invalid!')
-            );
-          }
+  if (command === 'play' || command === 'p') {
+    if (param.length > 0) {
+      if (msg.member.voice.channel !== null) {
+        if (ytdl.validateURL(param) === true) {
+          playback(msg.member.voice.channel, param);
+          ytdl.getBasicInfo(param)
+            .then((info) => {
+              const minutes = Math.floor(info.player_response.videoDetails.lengthSeconds / 60);
+              const seconds = info.player_response.videoDetails.lengthSeconds % 60;
+
+              msg.channel.send(
+                new Discord.MessageEmbed()
+                  .setColor('#00FF00')
+                  .setAuthor('--------------- ÎN CURS DE REDARE ---------------')
+                  .setTitle(info.player_response.videoDetails.title)
+                  .addFields(
+                    { name: 'Adăugat de', value: `<@${msg.author.id}>`, inline: true },
+                    { name: 'Durata', value: `${minutes}:${seconds}`, inline: true },
+                    {
+                      name: 'Link YouTube',
+                      value: `https://www.youtube.com/watch?v=${info.player_response.videoDetails.videoId}`,
+                    }
+                  )
+              );
+            })
+            .catch(console.log);
         } else {
           msg.channel.send(
             new Discord.MessageEmbed()
               .setColor('#FF0000')
-              .setTitle('Intră într-o cameră de voce că altfel o să ascult melodia singur!')
+              .setTitle('Link-ul introdus este invalid!')
           );
         }
       } else {
         msg.channel.send(
           new Discord.MessageEmbed()
-            .setColor('#00FF00')
-            .setTitle(`${prefix}play <link YouTube>`)
-            .setDescription(
-              `Redă sunetul din videoclipul introdus în camera curentă.\nVarianta scurtă a comenzii: ` +
-              `**${prefix}p <link YouTube>**`)
-            .addField('Exemple',
-              `${prefix}play <https://www.youtube.com/watch?v=dQw4w9WgXcQ>\n` +
-              `${prefix}p <https://youtube.com/watch?v=r_0JjYUe5jo>`)
+            .setColor('#FF0000')
+            .setTitle('Intră într-o cameră de voce că altfel o să ascult melodia singur!')
         );
       }
-      break;
-    default: break;
+    } else {
+      msg.channel.send(
+        new Discord.MessageEmbed()
+          .setColor('#00FF00')
+          .setTitle(`${prefix}play <link YouTube>`)
+          .setDescription(
+            `Redă sunetul din videoclipul introdus în camera curentă.\nVarianta scurtă a comenzii: ` +
+            `**${prefix}p <link YouTube>**`)
+          .addField('Exemple',
+            `${prefix}play <https://www.youtube.com/watch?v=dQw4w9WgXcQ>\n` +
+            `${prefix}p <https://youtube.com/watch?v=r_0JjYUe5jo>`)
+      );
+    }
+    return;
   }
 });
 
